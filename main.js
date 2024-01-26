127a8a11abb321e43d6c327fc283e056552dadcf
meocloud <112076209+meocloud1@users.noreply.github.com>
2024-01-27 03:39:13 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+// main.js
+import * as THREE from 'https://cdn.skypack.dev/three';
+
+let scene, camera, renderer, cube;
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('home').appendChild(renderer.domElement);
+
+    let geometry = new THREE.BoxGeometry();
+    let material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
